352e614a6d7a0ab2e43a0aca891c308b1df8b8cc
Slayder12 <gnutenko.s.r@gmail.com>
2025-01-30 18:19:57 +1000

    initial

---
+++ app/src/main/java/com/example/calculatingbmi/MainActivity.kt
@@ -0,0 +1,230 @@
+package com.example.calculatingbmi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            CalculationOfBMI()
+        }
+
+    }
+}
+
+@Composable
+fun CalculationOfBMI(modifier: Modifier = Modifier) {
+
+    val heightState = remember {
+        mutableIntStateOf(177)
+    }
+
+    val weightState = remember {
+        mutableIntStateOf(75)
+    }
+
+
+    val operationState = remember {
+        derivedStateOf {
+            Operation.calculateBmi(heightState.intValue, weightState.intValue)
+        }
+    }
+
+    val operationReferences = remember {
+        derivedStateOf {
+            Operation.resultReferences(operationState.value)
+        }
+    }
+
+    val reset = remember {
+        derivedStateOf {
+            weightState.intValue = 0
+            heightState.intValue = 0
+            operationState.value
+        }
+    }
+
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(11.dp)
+            .background(
+                color = Color.LightGray,
+                shape = RoundedCornerShape(12.dp)
+            )
+    ) {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(
+                    Color.DarkGray,
+                    shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp)
+                )
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(15.dp)
+                    .align(Alignment.Center),
+                text = "Калькулятор ИМП",
+                fontSize = 26.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Box(
+            modifier = modifier
+                .background(Color.LightGray)
+                .align(alignment = Alignment.CenterHorizontally)
+                .wrapContentHeight()
+        ) {
+            Column(
+                modifier = modifier
+
+            ) {
+                Box(
+                    modifier = Modifier
+                        .align(alignment = Alignment.CenterHorizontally)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .clickable {
+                                heightState.intValue++
+                            },
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Рост:",
+                            fontSize = 20.sp,
+                            color = Color.Black,
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = "${heightState.intValue} см",
+                            fontSize = 20.sp,
+                            color = Color.Black,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+
+                Box(
+                    modifier = Modifier
+                        .align(alignment = Alignment.CenterHorizontally)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .clickable {
+                                weightState.intValue++
+                            },
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Вес:",
+                            fontSize = 20.sp,
+                            color = Color.Black,
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = "${weightState.intValue} кг",
+                            fontSize = 20.sp,
+                            color = Color.Black,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+
+                Box(
+                    modifier = Modifier
+                        .background(
+                            Color.White,
+                            shape = RoundedCornerShape(15.dp)
+                        ),
+                )
+                Box(
+                    modifier = Modifier
+                        .align(alignment = Alignment.CenterHorizontally)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(5.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Коэффициент ИМТ:",
+                            fontSize = 20.sp,
+                            color = Color.Black,
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = operationState.value,
+                            fontSize = 20.sp,
+                            color = Color.Black,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+
+                Spacer(modifier = modifier.padding(top = 10.dp))
+
+                Box(
+                    modifier = Modifier
+                        .background(
+                            Color.White,
+                            shape = RoundedCornerShape(15.dp)
+                        )
+                        .align(alignment = Alignment.CenterHorizontally)
+                    ,
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(5.dp),
+                        text = operationReferences.value,
+                        fontSize = 20.sp,
+                        color = Color.Black,
+                    )
+                }
+            }
+        }
+        Text(
+            modifier = Modifier
+                .padding(15.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+                .clickable {
+                    reset.value
+                },
+            text = "Сбросить",
+            fontSize = 26.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+
---
+++ app/src/main/java/com/example/calculatingbmi/Operation.kt
@@ -0,0 +1,41 @@
+package com.example.calculatingbmi
+
+
+import java.text.DecimalFormat
+
+class Operation {
+
+    companion object {
+        fun calculateBmi(height: Int, weight: Int): String {
+            var result = "0"
+            if (weight == 0 || height == 0) {
+                return result
+            }
+            val bmi: Double = weight.toString().toDouble() / ((height.toString()
+                .toDouble() / 100) * (height.toString().toDouble() / 100))
+            val decimalFormat = DecimalFormat("#.0")
+            result = decimalFormat.format(bmi)
+            return result
+        }
+
+
+        fun resultReferences(bmi: String): String {
+            var result = ""
+            when {
+                bmi.toDouble() == 0.0 -> result = "Недостаточно данных!"
+                bmi.toDouble() in 1.0..16.0 -> result = "Выраженный дефицит массы тела"
+                bmi.toDouble() in 16.0..18.5 -> result = "Недостаточная масса тела"
+                bmi.toDouble() in 18.5..25.0 -> result = "Нормальная масса тела"
+                bmi.toDouble() in 25.5..30.0 -> result = "Избыточная масса тела(предожирение)"
+                bmi.toDouble() in 30.0..35.0 -> result = "Ожирение 1-ой степени"
+                bmi.toDouble() in 35.0..40.0 -> result = "Ожирение 2-ой степени"
+                bmi.toDouble() in 40.0..500.0 -> result = "Ожирение 3-ой степени"
+                bmi.toDouble() > 500.0 -> result = "Вы не человек"
+                else -> "Некорректные данные"
+
+            }
+            return result
+        }
+    }
+
+}
